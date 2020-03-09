8d4946131a97933c1777abc42ef947398a53c630
Teddy Brewer <tebrewer@jbi-ltd.com>
2020-03-09 16:32:56 -0500

    started Navbar development.  Gave background a dark red color and font a bright yellow.  Font color needs to be changed.

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div>
+            This is the Landing Page
+            Give information about myself
+        </div>
+    )
+}
+
+export default Landing
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const Navbar = () => {
+    return (
+        <Fragment>
+            <ul>
+                <li>
+                    <h1>Teddy Brewer</h1>
+                </li>
+            </ul>
+            Navbar Here
+        </Fragment>
+    )
+}
+
+export default Navbar
