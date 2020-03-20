6b1e51136fc1238cfb1cc8fbcb991bd838f8484b
Teddy Brewer <tebrewer@jbi-ltd.com>
2020-03-20 13:57:29 -0500

    creating and bringing in pages for paths in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import About from './components/layout/About'
+import Projects from './components/layout/Projects'
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
