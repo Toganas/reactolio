09692168e3f58fd6d6cf797356ef7f43a3f6af03
Teddy Brewer <tebrewer@jbi-ltd.com>
2020-03-20 13:58:44 -0500

    created Contact page and added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import About from './components/layout/About'
 import Projects from './components/layout/Projects'
+import Contact from './components/layout/Contact'
 import './App.css';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Switch>
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
       </Fragment>
     </Router>
