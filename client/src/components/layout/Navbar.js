bed27cbc472ae274a09fb6747983766ff061c24b
Teddy Brewer <tebrewer@jbi-ltd.com>
2020-04-01 14:59:03 -0500

    started using bootstrap to style navbar.  Think I'm going to look into Sidebars instead to accomplish what I want

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,31 +3,35 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>
-                        <h1>Teddy Brewer</h1>
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/about'>
-                        <h2>About</h2>
-                    </Link>
-                </li>
-                <li>
-                    <Link to='projects'>
-                        <h2>Projects</h2>
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/contact'>
-                        <h2>Contact</h2>
-                    </Link>
-                </li>
-            </ul>
+        <container fluid='md'>
+            <row>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to='/'>
+                                <h1>Teddy Brewer</h1>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>
+                                <h2>About</h2>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='projects'>
+                                <h2>Projects</h2>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/contact'>
+                                <h2>Contact</h2>
+                            </Link>
+                        </li>
+                    </ul>
 
-        </nav>
+                </nav>
+            </row>
+        </container>
     )
 }
 
