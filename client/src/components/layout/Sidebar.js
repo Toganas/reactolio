56d0168378d03b55aa74027c5de6f666149d10ae
Teddy Brewer <tebrewer@jbi-ltd.com>
2020-04-01 15:49:47 -0500

    Sidebar basics done.  Need to stylize it, make it more fitting for webpage.

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import styled from 'styled-components'
+import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom'
+
 /* This defines the actual bar going down the screen */
 const StyledSideNav = styled.div`
   position: fixed; 
@@ -10,24 +12,103 @@ const StyledSideNav = styled.div`
   top: 3.4em; 
   background-color: #222;
   overflow-x: hidden;
-  padding-top: 10px;
 `;
-class SideNav extends React.Component {
-    render() {
-        return (
-            <StyledSideNav>
 
-            </StyledSideNav>
-        )
+class SideNav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePath: 'props.location.pathname',
+            items: [
+                {
+                    path: '/',
+                    name: 'Home',
+                    css: 'fa fa-fw fa-home', /* font awesome */
+                    key: 1
+                },
+                {
+                    path: '/about',
+                    name: 'About',
+                    css: 'fa fa-fw fa-clock',
+                    key: 2
+                },
+                {
+                    path: '/NoMatch',
+                    name: 'NoMatch',
+                    css: 'fas fa-hashtag',
+                    key: 3
+                }
+            ]
+        }
     }
 
+    onItemClick = (path) => {
+        this.setState({ activePath: path })
+    }
+
+    render() {
+        const { items, activePath } = this.state;
+        return (
+            <StyledSideNav>
+                {
+                    items.map((item) => {
+                        return (
+                            <NavItem path={item.path} name={item.name} css={item.css} onItemClick={this.onItemClick} active={item.path === activePath} key={item.key} />
+                        )
+                    })
+                }
+            </StyledSideNav>
+        );
+
+    }
 }
+
+const RouterSideNav = withRouter(SideNav);
+
+const StyledNavItem = styled.div`
+height: 70px;
+width: 75px;
+text-align: center;
+margin-bottom: 0;
+a {
+    font-size: 2.7em;
+    color: ${(props) => props.active ? "white" : "#9FFFCV"};
+    : hover {
+    opacity: 0.7;
+    text - decoration: none;
+     }
+    }
+`;
+
+class NavItem extends React.Component {
+    handleClick = () => {
+        const { path, onItemClick } = this.props;
+        onItemClick(path);
+    }
+
+    render() {
+        const { active } = this.props;
+        return (
+            <StyledNavItem active={active}>
+                <Link to={this.props.path} className={this.props.css} onClick={this.handleClick}>
+                    <NavIcon></NavIcon>
+                </Link>
+            </StyledNavItem>
+        )
+    }
+}
+
+const NavIcon = styled.div`
+
+`;
+
+
 
 export const Sidebar = () => {
     return (
-        <SideNav>
+        <RouterSideNav>
 
-        </SideNav>
+        </RouterSideNav>
     )
 }
 
